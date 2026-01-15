5a2de8f67d429455155a90ebb5978befefc79527
Deepak <gxyz717@gmail.com>
2026-01-15 23:24:37 +0530

    build: updated dist for playwright bootstrap

--- dist/nodes/TwitterMediaUpload/TwitterMediaUpload.node.js
+++ dist/nodes/TwitterMediaUpload/TwitterMediaUpload.node.js
@@ -1,4 +1,37 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -7,6 +40,115 @@ exports.TwitterMediaUpload = void 0;
 const n8n_workflow_1 = require("n8n-workflow");
 const oauth_1_0a_1 = __importDefault(require("oauth-1.0a"));
 const crypto_js_1 = __importDefault(require("crypto-js"));
+const playwright_core_1 = require("playwright-core");
+const os = __importStar(require("os"));
+const path = __importStar(require("path"));
+// @ts-ignore
+const registry_1 = require("playwright-core/lib/server/registry");
+async function ensureChromium() {
+    const dir = path.join(os.homedir(), '.cache', 'ms-playwright');
+    const chromiumBrowser = registry_1.registry.findExecutable('chromium');
+    if (!chromiumBrowser.executablePath()) {
+        console.log('Downloading Chromium for Playwright...');
+        await chromiumBrowser.install(dir);
+    }
+}
+function extractTweetPayload(rawJson) {
+    var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k;
+    const result = (_b = (_a = rawJson === null || rawJson === void 0 ? void 0 : rawJson.data) === null || _a === void 0 ? void 0 : _a.tweetResult) === null || _b === void 0 ? void 0 : _b.result;
+    if (!result)
+        return null;
+    const legacy = result.legacy || {};
+    const viewsObj = result.views || {};
+    const noteTweet = ((_d = (_c = result.note_tweet) === null || _c === void 0 ? void 0 : _c.note_tweet_results) === null || _d === void 0 ? void 0 : _d.result) || {};
+    const entitiesLegacy = legacy.entities || {};
+    const entitiesNote = noteTweet.entity_set || {};
+    const user = ((_f = (_e = result.core) === null || _e === void 0 ? void 0 : _e.user_results) === null || _f === void 0 ? void 0 : _f.result) || {};
+    const userCore = user.core || {};
+    const userLegacy = user.legacy || {};
+    const userEntities = userLegacy.entities || {};
+    const tweetId = legacy.id_str || result.rest_id;
+    const username = userCore.screen_name || '';
+    const tweetUrl = tweetId && username ? `https://x.com/${username}/status/${tweetId}` : null;
+    const hashtagsNote = (entitiesNote.hashtags || []).map((h) => h.text);
+    const hashtagsLegacy = (entitiesLegacy.hashtags || []).map((h) => h.text);
+    const hashtags = hashtagsNote.length > 0 ? hashtagsNote : hashtagsLegacy;
+    const mentionsNote = (entitiesNote.user_mentions || []).map((m) => m.screen_name);
+    const mentionsLegacy = (entitiesLegacy.user_mentions || []).map((m) => m.screen_name);
+    const mentions = mentionsNote.length > 0 ? mentionsNote : mentionsLegacy;
+    const fullText = noteTweet.text || legacy.full_text;
+    const rawSource = result.source || '';
+    let sourceApp = null;
+    if (rawSource) {
+        try {
+            const gtIdx = rawSource.indexOf('>');
+            const ltIdx = rawSource.lastIndexOf('<');
+            if (gtIdx !== -1 && ltIdx !== -1 && ltIdx > gtIdx) {
+                sourceApp = rawSource.substring(gtIdx + 1, ltIdx);
+            }
+        }
+        catch {
+            sourceApp = rawSource;
+        }
+    }
+    let website = null;
+    const urlEntity = ((_g = userEntities.url) === null || _g === void 0 ? void 0 : _g.urls) || [];
+    if (urlEntity.length > 0) {
+        website = urlEntity[0].expanded_url;
+    }
+    const followers = userLegacy.followers_count || 0;
+    const likes = parseInt(legacy.favorite_count || 0);
+    const retweets = parseInt(legacy.retweet_count || 0);
+    const replies = parseInt(legacy.reply_count || 0);
+    const followersInt = parseInt(followers || 0);
+    let engagementRate = null;
+    if (followersInt > 0) {
+        engagementRate = (likes + retweets + replies) / followersInt;
+    }
+    const viewsCount = viewsObj.count ? parseInt(viewsObj.count) : null;
+    const tweet = {
+        id: tweetId,
+        url: tweetUrl,
+        created_at: legacy.created_at,
+        language: legacy.lang,
+        views: viewsCount,
+        likes,
+        retweets,
+        replies,
+        quotes: legacy.quote_count || 0,
+        bookmarks: legacy.bookmark_count || 0,
+        hashtags,
+        mentions,
+        source: sourceApp || rawSource,
+        full_text: fullText,
+        engagement_rate: engagementRate,
+    };
+    const author = {
+        user_id: user.rest_id || user.id,
+        username,
+        profile: username ? `https://x.com/${username}` : null,
+        bio: ((_h = user.profile_bio) === null || _h === void 0 ? void 0 : _h.description) || userLegacy.description,
+        location: (_j = user.location) === null || _j === void 0 ? void 0 : _j.location,
+        website,
+        followers: followersInt,
+        verified: Boolean(((_k = user.verification) === null || _k === void 0 ? void 0 : _k.verified) || user.is_blue_verified),
+    };
+    return { tweet, author };
+}
+function getNoteLength(rawJson) {
+    var _a, _b, _c, _d, _e, _f;
+    try {
+        const result = (_b = (_a = rawJson === null || rawJson === void 0 ? void 0 : rawJson.data) === null || _a === void 0 ? void 0 : _a.tweetResult) === null || _b === void 0 ? void 0 : _b.result;
+        const noteText = (_e = (_d = (_c = result === null || result === void 0 ? void 0 : result.note_tweet) === null || _c === void 0 ? void 0 : _c.note_tweet_results) === null || _d === void 0 ? void 0 : _d.result) === null || _e === void 0 ? void 0 : _e.text;
+        if (noteText)
+            return noteText.length;
+        const legacyText = ((_f = result === null || result === void 0 ? void 0 : result.legacy) === null || _f === void 0 ? void 0 : _f.full_text) || '';
+        return legacyText.length;
+    }
+    catch {
+        return 0;
+    }
+}
 class TwitterMediaUpload {
     constructor() {
         this.description = {
@@ -26,6 +168,11 @@ class TwitterMediaUpload {
                 {
                     name: 'twitterMediaUploadApi',
                     required: true,
+                    displayOptions: {
+                        show: {
+                            operation: ['upload'],
+                        },
+                    },
                 },
             ],
             properties: [
@@ -41,8 +188,40 @@ class TwitterMediaUpload {
                             description: 'Upload media file to Twitter',
                             action: 'Upload media file to Twitter',
                         },
+                        {
+                            name: 'Search Tweet Metrics',
+                            value: 'searchMetrics',
+                            description: 'Get detailed metrics for a tweet',
+                            action: 'Get tweet metrics and author information',
+                        },
                     ],
                     default: 'upload',
+                },
+                {
+                    displayName: 'Tweet URL',
+                    name: 'tweetUrl',
+                    type: 'string',
+                    default: '',
+                    required: true,
+                    displayOptions: {
+                        show: {
+                            operation: ['searchMetrics'],
+                        },
+                    },
+                    placeholder: 'https://x.com/username/status/1234567890',
+                    description: 'The full URL of the tweet to analyze',
+                },
+                {
+                    displayName: 'Timeout (ms)',
+                    name: 'timeout',
+                    type: 'number',
+                    default: 25000,
+                    displayOptions: {
+                        show: {
+                            operation: ['searchMetrics'],
+                        },
+                    },
+                    description: 'Maximum time to wait for page load (milliseconds)',
                 },
                 {
                     displayName: 'Binary Property',
@@ -118,27 +297,81 @@ class TwitterMediaUpload {
     async execute() {
         const items = this.getInputData();
         const returnData = [];
-        const credentials = await this.getCredentials('twitterMediaUploadApi');
-        // Setup OAuth1
-        const oauth = new oauth_1_0a_1.default({
-            consumer: {
-                key: credentials.consumerKey,
-                secret: credentials.consumerSecret,
-            },
-            signature_method: 'HMAC-SHA1',
-            hash_function(base_string, key) {
-                return crypto_js_1.default.HmacSHA1(base_string, key).toString(crypto_js_1.default.enc.Base64);
-            },
-        });
-        const token = {
-            key: credentials.accessToken,
-            secret: credentials.accessTokenSecret,
-        };
+        const operation = this.getNodeParameter('operation', 0);
+        // Only get credentials if operation is upload
+        let credentials;
+        let oauth;
+        let token;
+        if (operation === 'upload') {
+            credentials = await this.getCredentials('twitterMediaUploadApi');
+            // Setup OAuth1
+            oauth = new oauth_1_0a_1.default({
+                consumer: {
+                    key: credentials.consumerKey,
+                    secret: credentials.consumerSecret,
+                },
+                signature_method: 'HMAC-SHA1',
+                hash_function(base_string, key) {
+                    return crypto_js_1.default.HmacSHA1(base_string, key).toString(crypto_js_1.default.enc.Base64);
+                },
+            });
+            token = {
+                key: credentials.accessToken,
+                secret: credentials.accessTokenSecret,
+            };
+        }
         const TWITTER_UPLOAD_URL = 'https://upload.twitter.com/1.1/media/upload.json';
         for (let itemIndex = 0; itemIndex < items.length; itemIndex++) {
             try {
                 const operation = this.getNodeParameter('operation', itemIndex);
-                if (operation === 'upload') {
+                if (operation === 'searchMetrics') {
+                    const tweetUrl = this.getNodeParameter('tweetUrl', itemIndex);
+                    const timeout = this.getNodeParameter('timeout', itemIndex, 25000);
+                    let bestRaw = null;
+                    let bestLen = -1;
+                    await ensureChromium();
+                    const browser = await playwright_core_1.chromium.launch({ headless: true });
+                    const context = await browser.newContext();
+                    const page = await context.newPage();
+                    page.on('response', async (response) => {
+                        try {
+                            const url = response.url();
+                            if (!url.includes('TweetResultByRestId'))
+                                return;
+                            let data;
+                            try {
+                                data = await response.json();
+                            }
+                            catch {
+                                const body = await response.text();
+                                data = JSON.parse(body);
+                            }
+                            const length = getNoteLength(data);
+                            if (length > bestLen) {
+                                bestLen = length;
+                                bestRaw = data;
+                            }
+                        }
+                        catch (error) {
+                            // Ignore response parsing errors
+                        }
+                    });
+                    await page.goto(tweetUrl, { timeout });
+                    await page.waitForTimeout(5000);
+                    await browser.close();
+                    if (!bestRaw) {
+                        throw new n8n_workflow_1.NodeOperationError(this.getNode(), 'Could not extract tweet data. Please verify the URL is correct.', { itemIndex });
+                    }
+                    const payload = extractTweetPayload(bestRaw);
+                    if (!payload) {
+                        throw new n8n_workflow_1.NodeOperationError(this.getNode(), 'Failed to parse tweet data', { itemIndex });
+                    }
+                    returnData.push({
+                        json: payload,
+                        pairedItem: itemIndex,
+                    });
+                }
+                else if (operation === 'upload') {
                     const binaryPropertyName = this.getNodeParameter('binaryPropertyName', itemIndex);
                     const mediaType = this.getNodeParameter('mediaType', itemIndex);
                     const customMediaType = this.getNodeParameter('customMediaType', itemIndex, '');
